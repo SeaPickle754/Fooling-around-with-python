99429d010d8a194645b4ffc7f68ebf479c7f2881
PythonCodeingKing <62182495+PythonCodeingKing@users.noreply.github.com>
2020-05-15 14:05:28 -0600

    Add files via upload
    This is the code. It works only in python3 with matplotlib.pyplot installed. It keeps asking you for a seed until you enter'0000'.
    I know it works in IDLE, but some editors like Visual Studio might not work. If you have a request for me, or an issue with my code, feel free to leave a comment or issue.

---
+++ 0003.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import random
+
+def main():
+    while True:
+        numbers=[]
+        seed_input=int(input('Enter A seed: '))
+        if seed_input ==0000:
+            break
+        random.seed(seed_input)
+        for i in range(1,100):
+            rand=random.randint(1,100)
+            numbers.append(rand)
+        fig,ax=plt.subplots()
+        plt.plot(numbers)
+        plt.show()
+main()
