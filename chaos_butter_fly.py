a86cc960174b9f5ba6a9118a3e1872a065b2a625
PythonCodeingKing <62182495+PythonCodeingKing@users.noreply.github.com>
2020-05-16 09:59:16 -0600

    Update and rename 0003.py to chaos_butter_fly.py

--- chaos_butter_fly.py
+++ chaos_butter_fly.py
@@ -5,7 +5,7 @@ def main():
     while True:
         numbers=[]
         seed_input=int(input('Enter A seed: '))
-        if seed_input ==0000:
+        if seed_input == 0000:
             break
         random.seed(seed_input)
         for i in range(1,100):
